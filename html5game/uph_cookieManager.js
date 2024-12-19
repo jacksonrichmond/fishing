f7ccbfb3c4e9b08c1704c897d2d4af60f6903235
jacksonrichmond <jrichmond@hsstu.lpsb.org>
2024-12-19 11:18:28 -0600

    Update uph_cookieManager.js

--- html5game/uph_cookieManager.js
+++ html5game/uph_cookieManager.js
@@ -66,15 +66,3 @@ function cookieExsists(argument0)
 }
 
 
-
-
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-setCookie('Gone FishinghookChosenID','0',7);
